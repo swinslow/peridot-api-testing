021db7b57c02679d1b51c59e20741c18e6698bfc
Steve Winslow <swinslow@gmail.com>
2019-06-23 12:21:57 -0400

    Add subprojects sub get and post tests
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

--- test/endpoints/subprojects.go
+++ test/endpoints/subprojects.go
@@ -11,10 +11,12 @@ func getSubprojectsTests() []testresult.TestFunc {
 	return []testresult.TestFunc{
 		subprojectsGetViewer,
 		subprojectsPostOperator,
+		subprojectsSubGetViewer,
+		subprojectsSubPostOperator,
 	}
 }
 
-// ===== GET /projects
+// ===== GET /subprojects
 
 func subprojectsGetViewer(root string) *testresult.TestResult {
 	res := &testresult.TestResult{
@@ -66,6 +68,73 @@ func subprojectsPostOperator(root string) *testresult.TestResult {
 
 	// now, confirm that a new project was actually added
 	res.Wanted = `{"subprojects":[{"id":1,"project_id":2,"name":"blorple","fullname":"The blorple Subproject"},{"id":2,"project_id":2,"name":"filfre","fullname":"The filfre Subproject"},{"id":3,"project_id":2,"name":"fweep","fullname":"The fweep Subproject"},{"id":4,"project_id":3,"name":"girgol","fullname":"The girgol Subproject"},{"id": 5, "project_id": 3, "name": "plugh", "fullname": "The plugh Subproject"}]}`
+	err = utils.GetContent(res, "3", url, 200, "operator")
+	if err != nil {
+		return res
+	}
+
+	if !utils.IsMatch(res) {
+		utils.FailMatch(res, "4")
+		return res
+	}
+
+	utils.Pass(res)
+	return res
+}
+
+// ===== GET /projects/2/subprojects
+
+func subprojectsSubGetViewer(root string) *testresult.TestResult {
+	res := &testresult.TestResult{
+		Suite:   "endpoints",
+		Element: "projects/{id}/subprojects",
+		ID:      "GET (viewer)",
+	}
+
+	url := root + "/projects/2/subprojects"
+
+	res.Wanted = `{"subprojects":[{"id":1,"project_id":2,"name":"blorple","fullname":"The blorple Subproject"},{"id":2,"project_id":2,"name":"filfre","fullname":"The filfre Subproject"},{"id":3,"project_id":2,"name":"fweep","fullname":"The fweep Subproject"}]}`
+	err := utils.GetContent(res, "1", url, 200, "viewer")
+	if err != nil {
+		return res
+	}
+
+	if !utils.IsMatch(res) {
+		utils.FailMatch(res, "2")
+		return res
+	}
+
+	utils.Pass(res)
+	return res
+}
+
+// ===== POST /projects/2/subprojects
+
+func subprojectsSubPostOperator(root string) *testresult.TestResult {
+	res := &testresult.TestResult{
+		Suite:   "endpoints",
+		Element: "subprojects",
+		ID:      "POST (operator)",
+	}
+
+	url := root + "/projects/2/subprojects"
+
+	// first, send POST to add a new project
+	body := `{"name": "plugh", "fullname": "The plugh Subproject"}`
+	res.Wanted = `{"id": 5}`
+	err := utils.Post(res, "1", url, body, 201, "operator")
+	if err != nil {
+		return res
+	}
+
+	if !utils.IsMatch(res) {
+		utils.FailMatch(res, "2")
+		return res
+	}
+
+	// now, confirm that a new project was actually added
+	url = root + "/subprojects"
+	res.Wanted = `{"subprojects":[{"id":1,"project_id":2,"name":"blorple","fullname":"The blorple Subproject"},{"id":2,"project_id":2,"name":"filfre","fullname":"The filfre Subproject"},{"id":3,"project_id":2,"name":"fweep","fullname":"The fweep Subproject"},{"id":4,"project_id":3,"name":"girgol","fullname":"The girgol Subproject"},{"id": 5, "project_id": 2, "name": "plugh", "fullname": "The plugh Subproject"}]}`
 	err = utils.GetContent(res, "3", url, 200, "operator")
 	if err != nil {
 		return res
